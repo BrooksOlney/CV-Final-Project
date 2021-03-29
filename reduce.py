5f6daae17623ab70c9d8b17a7272ee865efca3c2
BrooksOlney <brooks.olney@gmail.com>
2021-03-29 18:59:35 -0400

    start of lra changes, likely to be migrated to other file

--- reduce.py
+++ reduce.py
@@ -7,7 +7,7 @@
 
     This is the main script that implements and evaluates the reduction methods.
     Pipeline is as follows:
-        1) loaded model and dataset are
+        1) load model and dataset
         2) compute baseline metrics for accuracy, model size, and performance (time for inference)
         3) perform model reduction technique - pruning or rank factorization
       **4) compute (2) for reduced model
@@ -19,10 +19,38 @@
 """
 
 import mnist
+import numpy as np
+
+def low_rank_factorization(model):
+
+    for layer in model.layers:
+        if "dense" in layer.name:
+            newLayers = lrf_fc_layer(layer)
+
+
+def lrf_fc_layer(layer):
+
+    w, b = layer.get_weights()
+    t = w.shape[0]
+
+    U, S, V = np.linalg.svd(w)
+
+    Ut = U[:, :t]
+    St = S[:t]
+    Vt = V[:t, :]
+
+    L = np.dot(np.diag(St), Vt)
+
+
+
+    print("hi")
+
+
 
 
 if __name__ == "__main__":
     model = mnist.build_model()
-    trainX, trainY, testX, testY = mnist.load_data()
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    low_rank_factorization(model)
 
     print(model.summary())
